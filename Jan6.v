fefe848e29092d9caf80fb8f4f875a44d21a6ab1
CharlesCNorton <machineelv@gmail.com>
2026-01-07 19:12:28 -0500

    Add legal framework, electoral data, and substantive proofs

--- Jan6.v
+++ Jan6.v
@@ -600,3 +600,639 @@ Proof.
   unfold time_03_40_AM_next, time_01_00_PM.
   reflexivity.
 Defined.
+
+Inductive CauseOfDeath : Type
+  := Gunshot : CauseOfDeath
+   | DrugOverdose : CauseOfDeath
+   | Stroke : CauseOfDeath
+   | HeartAttack_COD : CauseOfDeath
+   | Suicide : CauseOfDeath
+   | Trampling : CauseOfDeath.
+
+Inductive DeathClassification : Type
+  := Homicide : DeathClassification
+   | Accident : DeathClassification
+   | Natural : DeathClassification
+   | SuicideClass : DeathClassification
+   | Undetermined : DeathClassification.
+
+Record Decedent : Type
+  := mkDecedent
+       { decedent_name : string
+       ; decedent_category : ActorCategory
+       ; death_location : Location
+       ; death_cause : CauseOfDeath
+       ; death_classification : DeathClassification
+       ; death_date_offset : nat
+       }.
+
+Definition decedent_babbitt : Decedent
+  := mkDecedent
+       "Ashli Babbitt"%string
+       Rioter
+       SpeakerLobby
+       Gunshot
+       Homicide
+       0.
+
+Definition decedent_boyland : Decedent
+  := mkDecedent
+       "Rosanne Boyland"%string
+       Rioter
+       LowerWestTerrace
+       Trampling
+       Accident
+       0.
+
+Definition decedent_greeson : Decedent
+  := mkDecedent
+       "Kevin Greeson"%string
+       Rioter
+       WestFront
+       HeartAttack_COD
+       Natural
+       0.
+
+Definition decedent_phillips : Decedent
+  := mkDecedent
+       "Benjamin Phillips"%string
+       Rioter
+       WestFront
+       Stroke
+       Natural
+       0.
+
+Definition decedent_sicknick : Decedent
+  := mkDecedent
+       "Brian Sicknick"%string
+       CapitolPolice
+       WestFront
+       Stroke
+       Natural
+       1.
+
+Definition jan6_decedents : list Decedent
+  := [ decedent_babbitt
+     ; decedent_boyland
+     ; decedent_greeson
+     ; decedent_phillips
+     ; decedent_sicknick
+     ].
+
+Definition deaths_within_36_hours : nat := 5.
+
+Inductive PipeBombLocation : Type
+  := RNCHeadquarters : PipeBombLocation
+   | DNCHeadquarters : PipeBombLocation.
+
+Record PipeBombEvent : Type
+  := mkPipeBomb
+       { bomb_location : PipeBombLocation
+       ; placed_night_before : bool
+       ; discovery_time : Minute
+       ; neutralization_time : Minute
+       }.
+
+Definition time_01_05_PM : Minute := 785.
+Definition time_03_33_PM : Minute := 933.
+Definition time_04_36_PM : Minute := 996.
+
+Definition rnc_pipe_bomb : PipeBombEvent
+  := mkPipeBomb
+       RNCHeadquarters
+       true
+       time_01_05_PM
+       time_03_33_PM.
+
+Definition dnc_pipe_bomb : PipeBombEvent
+  := mkPipeBomb
+       DNCHeadquarters
+       true
+       time_01_05_PM
+       time_04_36_PM.
+
+Definition pipe_bombs : list PipeBombEvent
+  := [rnc_pipe_bomb; dnc_pipe_bomb].
+
+Lemma both_bombs_placed_night_before
+  : forallb placed_night_before pipe_bombs = true.
+Proof.
+  unfold pipe_bombs.
+  simpl.
+  reflexivity.
+Defined.
+
+Lemma rnc_bomb_neutralized_before_dnc
+  : neutralization_time rnc_pipe_bomb < neutralization_time dnc_pipe_bomb.
+Proof.
+  unfold rnc_pipe_bomb, dnc_pipe_bomb.
+  simpl.
+  unfold time_03_33_PM, time_04_36_PM.
+  lia.
+Defined.
+
+Inductive State : Type
+  := Alabama | Alaska | Arizona | Arkansas | California
+   | Colorado | Connecticut | Delaware | Florida | Georgia
+   | Hawaii | Idaho | Illinois | Indiana | Iowa
+   | Kansas | Kentucky | Louisiana | Maine | Maryland
+   | Massachusetts | Michigan | Minnesota | Mississippi | Missouri
+   | Montana | Nebraska | Nevada | NewHampshire | NewJersey
+   | NewMexico | NewYork | NorthCarolina | NorthDakota | Ohio
+   | Oklahoma | Oregon | Pennsylvania | RhodeIsland | SouthCarolina
+   | SouthDakota | Tennessee | Texas | Utah | Vermont
+   | Virginia | Washington | WestVirginia | Wisconsin | Wyoming
+   | DistrictOfColumbia.
+
+Inductive Candidate : Type
+  := JoeBiden : Candidate
+   | DonaldTrump_Cand : Candidate.
+
+Record StateResult : Type
+  := mkStateResult
+       { state : State
+       ; electoral_votes : nat
+       ; winner : Candidate
+       ; objection_filed : bool
+       ; objection_sustained : bool
+       }.
+
+Definition arizona_result : StateResult
+  := mkStateResult Arizona 11 JoeBiden true false.
+
+Definition pennsylvania_result : StateResult
+  := mkStateResult Pennsylvania 20 JoeBiden true false.
+
+Definition georgia_result : StateResult
+  := mkStateResult Georgia 16 JoeBiden false false.
+
+Definition michigan_result : StateResult
+  := mkStateResult Michigan 16 JoeBiden false false.
+
+Definition wisconsin_result : StateResult
+  := mkStateResult Wisconsin 10 JoeBiden false false.
+
+Definition nevada_result : StateResult
+  := mkStateResult Nevada 6 JoeBiden false false.
+
+Definition contested_states : list StateResult
+  := [ arizona_result
+     ; pennsylvania_result
+     ; georgia_result
+     ; michigan_result
+     ; wisconsin_result
+     ; nevada_result
+     ].
+
+Definition biden_total_ev : nat := 306.
+Definition trump_total_ev : nat := 232.
+Definition ev_to_win : nat := 270.
+
+Lemma biden_exceeded_threshold
+  : biden_total_ev >= ev_to_win.
+Proof.
+  unfold biden_total_ev, ev_to_win.
+  lia.
+Defined.
+
+Lemma trump_below_threshold
+  : trump_total_ev < ev_to_win.
+Proof.
+  unfold trump_total_ev, ev_to_win.
+  lia.
+Defined.
+
+Definition states_with_objections : list StateResult
+  := filter objection_filed contested_states.
+
+Lemma two_states_had_objections
+  : List.length states_with_objections = 2.
+Proof.
+  unfold states_with_objections, contested_states.
+  simpl.
+  reflexivity.
+Defined.
+
+Lemma no_objections_sustained
+  : forallb (fun s => negb (objection_sustained s)) contested_states = true.
+Proof.
+  unfold contested_states.
+  simpl.
+  reflexivity.
+Defined.
+
+Module TwelfthAmendment.
+
+  Inductive ElectoralCertificateStatus : Type
+    := Sealed : ElectoralCertificateStatus
+     | Transmitted : ElectoralCertificateStatus
+     | Opened : ElectoralCertificateStatus
+     | Counted : ElectoralCertificateStatus.
+
+  Record ElectoralCertificate : Type
+    := mkCertificate
+         { cert_state : State
+         ; cert_ev_count : nat
+         ; cert_president_vote : Candidate
+         ; cert_vp_vote : NamedActor
+         ; cert_status : ElectoralCertificateStatus
+         }.
+
+  Definition president_of_senate_opens
+    (cert : ElectoralCertificate)
+    : ElectoralCertificate
+    := mkCertificate
+         (cert_state cert)
+         (cert_ev_count cert)
+         (cert_president_vote cert)
+         (cert_vp_vote cert)
+         Opened.
+
+  Definition count_certificate
+    (cert : ElectoralCertificate)
+    : ElectoralCertificate
+    := mkCertificate
+         (cert_state cert)
+         (cert_ev_count cert)
+         (cert_president_vote cert)
+         (cert_vp_vote cert)
+         Counted.
+
+  Definition majority_threshold (total_ev : nat)
+    : nat
+    := (total_ev / 2) + 1.
+
+  Definition has_majority (candidate_ev total_ev : nat)
+    : bool
+    := Nat.leb (majority_threshold total_ev) candidate_ev.
+
+  Definition total_electoral_votes : nat := 538.
+
+  Lemma majority_is_270
+    : majority_threshold total_electoral_votes = 270.
+  Proof.
+    unfold majority_threshold, total_electoral_votes.
+    simpl.
+    reflexivity.
+  Defined.
+
+  Lemma biden_has_majority
+    : has_majority biden_total_ev total_electoral_votes = true.
+  Proof.
+    unfold has_majority, majority_threshold.
+    unfold biden_total_ev, total_electoral_votes.
+    simpl.
+    reflexivity.
+  Defined.
+
+  Definition vp_role_is_ministerial : Prop
+    := forall cert : ElectoralCertificate,
+       cert_status (president_of_senate_opens cert) = Opened.
+
+  Lemma vp_opens_not_decides
+    : vp_role_is_ministerial.
+  Proof.
+    unfold vp_role_is_ministerial.
+    intros cert.
+    unfold president_of_senate_opens.
+    simpl.
+    reflexivity.
+  Defined.
+
+End TwelfthAmendment.
+
+Module USC3_Section15.
+
+  Definition statutory_start_time : Minute := time_01_00_PM.
+
+  Inductive ObjectionRequirement : Type
+    := OneSenator : ObjectionRequirement
+     | OneRepresentative : ObjectionRequirement.
+
+  Definition valid_objection_requires : list ObjectionRequirement
+    := [OneSenator; OneRepresentative].
+
+  Record Objection : Type
+    := mkObjection
+         { objected_state : State
+         ; senator_signature : bool
+         ; representative_signature : bool
+         ; house_vote_to_sustain : bool
+         ; senate_vote_to_sustain : bool
+         }.
+
+  Definition objection_is_valid (obj : Objection)
+    : bool
+    := andb (senator_signature obj) (representative_signature obj).
+
+  Definition objection_sustained (obj : Objection)
+    : bool
+    := andb (house_vote_to_sustain obj) (senate_vote_to_sustain obj).
+
+  Definition arizona_objection : Objection
+    := mkObjection
+         Arizona
+         true
+         true
+         false
+         false.
+
+  Definition pennsylvania_objection : Objection
+    := mkObjection
+         Pennsylvania
+         true
+         true
+         false
+         false.
+
+  Definition jan6_objections : list Objection
+    := [arizona_objection; pennsylvania_objection].
+
+  Lemma both_objections_valid
+    : forallb objection_is_valid jan6_objections = true.
+  Proof.
+    unfold jan6_objections, objection_is_valid.
+    simpl.
+    reflexivity.
+  Defined.
+
+  Lemma neither_objection_sustained
+    : forallb objection_sustained jan6_objections = false.
+  Proof.
+    unfold jan6_objections, objection_sustained.
+    simpl.
+    reflexivity.
+  Defined.
+
+  Definition max_debate_minutes : nat := 120.
+
+  Inductive JointSessionState : Type
+    := Convened : JointSessionState
+     | ReadingCertificates : JointSessionState
+     | ObjectionRaised : JointSessionState
+     | ChambersSeparated : JointSessionState
+     | Debating : JointSessionState
+     | Voting : JointSessionState
+     | Reconvened : JointSessionState
+     | Completed : JointSessionState
+     | Interrupted : JointSessionState.
+
+  Record JointSessionEvent : Type
+    := mkSessionEvent
+         { session_time : Minute
+         ; session_state : JointSessionState
+         }.
+
+  Definition jan6_session_events : list JointSessionEvent
+    := [ mkSessionEvent time_01_00_PM Convened
+       ; mkSessionEvent time_02_13_PM Interrupted
+       ; mkSessionEvent time_08_00_PM Reconvened
+       ; mkSessionEvent time_03_40_AM_next Completed
+       ].
+
+  Lemma session_was_interrupted
+    : existsb (fun e =>
+        match session_state e with
+        | Interrupted => true
+        | _ => false
+        end) jan6_session_events = true.
+  Proof.
+    unfold jan6_session_events.
+    simpl.
+    reflexivity.
+  Defined.
+
+  Lemma session_eventually_completed
+    : existsb (fun e =>
+        match session_state e with
+        | Completed => true
+        | _ => false
+        end) jan6_session_events = true.
+  Proof.
+    unfold jan6_session_events.
+    simpl.
+    reflexivity.
+  Defined.
+
+End USC3_Section15.
+
+Module SeditiousConspiracyAnalysis.
+
+  Definition force_was_used : Prop
+    := total_officers_injured > 0.
+
+  Lemma force_element_satisfied
+    : force_was_used.
+  Proof.
+    unfold force_was_used, total_officers_injured.
+    lia.
+  Defined.
+
+  Definition official_proceeding_existed : Prop
+    := session_start jan6_certification = time_01_00_PM.
+
+  Lemma proceeding_element_satisfied
+    : official_proceeding_existed.
+  Proof.
+    unfold official_proceeding_existed, jan6_certification.
+    simpl.
+    reflexivity.
+  Defined.
+
+  Definition proceeding_was_obstructed : Prop
+    := session_interrupted jan6_certification = true.
+
+  Lemma obstruction_element_satisfied
+    : proceeding_was_obstructed.
+  Proof.
+    unfold proceeding_was_obstructed.
+    apply certification_was_interrupted.
+  Defined.
+
+  Definition multiple_persons_involved : Prop
+    := List.length all_seditious_convictions >= 2.
+
+  Lemma conspiracy_element_satisfied
+    : multiple_persons_involved.
+  Proof.
+    unfold multiple_persons_involved, all_seditious_convictions.
+    simpl.
+    lia.
+  Defined.
+
+  Definition us_property_was_seized : Prop
+    := exists e : Event,
+       event_type e = BuildingBreach /\
+       In e timeline.
+
+  Lemma seizure_element_satisfied
+    : us_property_was_seized.
+  Proof.
+    unfold us_property_was_seized.
+    exists event_building_breach.
+    split.
+    - unfold event_building_breach.
+      simpl.
+      reflexivity.
+    - unfold timeline.
+      simpl.
+      right. right. right. right. right. right. right.
+      left.
+      reflexivity.
+  Defined.
+
+  Definition breach_interrupted_certification : Prop
+    := event_time event_building_breach =
+       match interruption_time jan6_certification with
+       | Some t => t
+       | None => 0
+       end.
+
+  Lemma breach_caused_interruption
+    : breach_interrupted_certification.
+  Proof.
+    unfold breach_interrupted_certification.
+    unfold event_building_breach, jan6_certification.
+    simpl.
+    reflexivity.
+  Defined.
+
+  Definition conviction_rate_for_sedition : nat
+    := List.length all_seditious_convictions.
+
+  Lemma six_seditious_convictions
+    : conviction_rate_for_sedition = 6.
+  Proof.
+    unfold conviction_rate_for_sedition, all_seditious_convictions.
+    simpl.
+    reflexivity.
+  Defined.
+
+  Definition total_conviction_years : nat
+    := total_seditious_sentence_months / 12.
+
+  Lemma over_100_years_sentences
+    : total_conviction_years >= 100.
+  Proof.
+    unfold total_conviction_years, total_seditious_sentence_months.
+    unfold all_seditious_convictions.
+    simpl.
+    lia.
+  Defined.
+
+  Definition delay_enabled_breach : Prop
+    := match guard_delay_minutes jan6_guard_request with
+       | Some delay => delay > 60
+       | None => False
+       end.
+
+  Lemma significant_guard_delay
+    : delay_enabled_breach.
+  Proof.
+    unfold delay_enabled_breach.
+    unfold guard_delay_minutes, jan6_guard_request.
+    simpl.
+    unfold time_04_32_PM, time_01_49_PM.
+    lia.
+  Defined.
+
+End SeditiousConspiracyAnalysis.
+
+Module Summary.
+
+  Definition event_count : nat := List.length timeline.
+  Definition conviction_count : nat := List.length all_seditious_convictions.
+  Definition death_count : nat := List.length jan6_decedents.
+  Definition injury_count : nat := total_officers_injured.
+  Definition pipe_bomb_count : nat := List.length pipe_bombs.
+
+  Lemma event_timeline_has_17_events
+    : event_count = 17.
+  Proof.
+    unfold event_count, timeline.
+    simpl.
+    reflexivity.
+  Defined.
+
+  Lemma five_deaths_recorded
+    : death_count = 5.
+  Proof.
+    unfold death_count, jan6_decedents.
+    simpl.
+    reflexivity.
+  Defined.
+
+  Lemma one_hundred_forty_officers_injured
+    : injury_count = 140.
+  Proof.
+    unfold injury_count, total_officers_injured.
+    reflexivity.
+  Defined.
+
+  Lemma two_pipe_bombs_placed
+    : pipe_bomb_count = 2.
+  Proof.
+    unfold pipe_bomb_count, pipe_bombs.
+    simpl.
+    reflexivity.
+  Defined.
+
+  Definition breach_duration_minutes : nat
+    := time_06_00_PM - time_02_13_PM.
+
+  Definition session_delay_minutes : nat
+    := time_03_40_AM_next - time_01_00_PM.
+
+  Definition guard_response_minutes : nat
+    := time_05_20_PM - time_01_49_PM.
+
+  Lemma breach_lasted_227_minutes
+    : breach_duration_minutes = 227.
+  Proof.
+    unfold breach_duration_minutes.
+    unfold time_06_00_PM, time_02_13_PM.
+    reflexivity.
+  Defined.
+
+  Lemma certification_delayed_880_minutes
+    : session_delay_minutes = 880.
+  Proof.
+    unfold session_delay_minutes.
+    unfold time_03_40_AM_next, time_01_00_PM.
+    reflexivity.
+  Defined.
+
+  Lemma guard_took_211_minutes
+    : guard_response_minutes = 211.
+  Proof.
+    unfold guard_response_minutes.
+    unfold time_05_20_PM, time_01_49_PM.
+    reflexivity.
+  Defined.
+
+  Definition longest_sentence_months : nat
+    := sentence_months conviction_tarrio.
+
+  Definition longest_sentence_years : nat
+    := longest_sentence_months / 12.
+
+  Lemma tarrio_got_22_years
+    : longest_sentence_years = 22.
+  Proof.
+    unfold longest_sentence_years, longest_sentence_months.
+    unfold conviction_tarrio.
+    simpl.
+    reflexivity.
+  Defined.
+
+  Definition certification_completed : bool
+    := session_completed jan6_certification.
+
+  Lemma democracy_prevailed
+    : certification_completed = true.
+  Proof.
+    unfold certification_completed.
+    apply certification_eventually_completed.
+  Defined.
+
+End Summary.
